2a834bc1077296b7bb6497e849ffa6b7bcee9a2f
Andy Keogh <Andykeoghleic@gmail.com>
2021-12-06 20:38:11 +0000

    added no user err

---
+++ components/UI/Warnings/NoUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NoUser = () => {
+  return (
+    <div className="flex flex-col text-center mt-10">
+      <p className="mt-1 text-4xl font-christmasFont tracking-widest font-black  uppercase text-christmasGold sm:text-5xl sm:tracking-tight lg:text-6xl">
+        Ho! Ho! No!
+      </p>
+      <p className="mt-10 text-4xl font-christmasFont tracking-widest font-black  uppercase text-christmasGold sm:text-5xl sm:tracking-tight lg:text-6xl">
+        Please Log In To Post A Joke
+      </p>
+    </div>
+  );
+};
+
+export default NoUser;
