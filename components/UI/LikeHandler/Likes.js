b00e7057bd3c1b5309e647caf823ef04cfac2a83
Andy Keogh <Andykeoghleic@gmail.com>
2021-12-06 20:38:21 +0000

    fixed likes

---
+++ components/UI/LikeHandler/Likes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { AiTwotoneHeart } from "react-icons/ai";
+import { FaPoop } from "react-icons/fa";
+import { supabase } from "../../../utils/supabaseClient";
+
+const Likes = ({ fav, row }) => {
+  const incrementCount = async (row_id) => {
+    const { data, error } = await supabase.rpc("increment", { row_id: row_id });
+  };
+  const decrementCount = async (row_id) => {
+    const { data, error } = await supabase.rpc("decrement", { row_id: row_id });
+  };
+
+  return (
+    <div className="flex justify-center align-top text-white">
+      <div className="pl-0 pr-3 h-4 w-4 text-santaRed">
+        <button onClick={() => incrementCount(row)}>
+          <AiTwotoneHeart />
+        </button>
+      </div>
+      <p className="ml-3 text-christmasGold">{fav}</p>
+      <div className="pl-3 pr-3 h-3 w-3 text-poop ">
+        <button onClick={() => decrementCount(row)}>
+          <FaPoop />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Likes;
