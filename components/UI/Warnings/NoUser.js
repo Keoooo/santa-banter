cbccd76f02f774dca4256d57330666093db21c4f
Andy Keogh <Andykeoghleic@gmail.com>
2022-03-20 12:03:00 +0700

    STYLE: fix margin

--- components/UI/Warnings/NoUser.js
+++ components/UI/Warnings/NoUser.js
@@ -7,7 +7,7 @@ const NoUser = () => {
         <p className="mt-1 text-4xl font-christmasFont tracking-widest font-black  uppercase text-christmasGold sm:text-5xl sm:tracking-tight lg:text-6xl">
           Ho! Ho! No!
         </p>
-        <p className=" text-4xl font-christmasFont tracking-widest font-black  uppercase text-christmasGold sm:text-5xl sm:tracking-tight lg:text-6xl">
+        <p className=" text-4xl mt-6 font-christmasFont tracking-widest font-black  uppercase text-christmasGold sm:text-5xl sm:tracking-tight lg:text-6xl">
           Please Log In To Post A Joke
         </p>
       </div>
